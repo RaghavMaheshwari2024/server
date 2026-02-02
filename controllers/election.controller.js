95b928877f88ff933df18ffe69abf513eff5cd3b
RaghavMaheshwari2024 <raghavmaheshwari.2028@gmail.com>
2026-02-02 19:09:09 +0530

    Added election.controller

---
+++ controllers/election.controller.js
@@ -0,0 +1,44 @@
+import prisma from '../config/db';
+
+exports.createElection = async (req, res) => {
+    try{
+        const {electionID} = req.body;
+        const newElection = await prisma.election.create({
+            data: {
+                electionID,
+                status: "CREATED"
+            }
+        });
+        res.status(201).json(newElection);
+    }catch(err){
+        res.status(500).json({error: err.message});
+    }
+};
+
+exports.updateElectionStatus = async (req, res) => {
+    try{
+        const {electionID, status} = req.body;
+        const updated = await prisma.election.update({
+            where: { electionID },
+            data: { status }
+        });
+
+        res.status(200).json(updated);
+
+    }catch(err){
+        res.status(500).json({error: err.message});
+    }
+}
+
+exports.getElection = async (req, res) => {
+    try{
+        const {electionID} = req.params;
+        const election = await prisma.election.findUnique({
+            where: { electionID }
+        });
+        res.status(200).json(election);
+    }catch(err){
+        res.status(500).json({error: err.message});
+    }
+}
+
